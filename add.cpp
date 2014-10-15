2ccfebcd370b80bc9fd7aebcc07c68cf2cfdb099
Ajay Negi <tannzin@gmail.com>
2014-10-15 17:59:48 +0000

    Create add.cpp
    addition of two numbers usin cpp...

---
+++ add.cpp
@@ -0,0 +1,11 @@
+// adding two numbers
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main ()
+{
+  int a,b;
+  cout<<"enter the numbers to be added";
+  cin>>a>>b;
+  cout<<a+b;
+}
